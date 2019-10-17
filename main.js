9e4e4ff3c15596588bd78ba9aaec3fed02ded7bd
Luciano Passalenti <luchontandil3@gmail.com>
2019-10-18 01:09:17 +0200

    Animaciones extras

--- main.js
+++ main.js
@@ -11,49 +11,22 @@ for (var i = 0; i < arr_mapa.length; i++) {
   arr_mapa[i] = new Array(_COL);
 }
 
-// Set the size of the map to fit the window
-function resize() {
-  // document.getElementById('mapa').style.height = window.innerHeight +"px";
-  document.getElementById('mapa').style.gridTemplateRows = "repeat("+_FILAS+","+(window.innerHeight/_FILAS)+"px)";
-  document.getElementById('mapa').style.gridTemplateColumns = "repeat("+_COL+","+(window.innerWidth/_COL)+"px)";
-  // Character size
-  document.getElementsByClassName('personaje')[0].style.setProperty('--height-character',((window.innerHeight/_FILAS)*2.2+'px'));
-  // Character margin
-  document.getElementsByClassName('personaje')[0].style.setProperty('--margin-character',(-window.innerHeight/_FILAS+'px'+" 0px 0px 0px"));
-
-}
-// Resize de las pisadas
-function resizeElements() {
-
-  aux_array = document.getElementsByClassName('pisado');
-
-  for (var i = 0; i < aux_array.length; i++) {
-    // Pisada size
-    aux_array[i].style.setProperty('--height-pisado',((window.innerHeight/_FILAS)*2+'px'));
-    // Pisada margin
-    aux_array[i].style.setProperty('--margin-pisado',(-(window.innerHeight/_FILAS)+'px'+" 0px 0px 0px"));
-  }
-
-
-
-}
-
 // Mapa inicial
   var mapa = [
               [0,'S',0,0,0, 0,0,0,0,0, 0,0,0,0,'V', 0,0,0,0,0, 0,0,0],
               [0,0,0,0,0, 0,0,0,0,1, 0,0,0,0,0, 0,0,0,0,0, 0,0,0],
               [0,2,2,2,2, 2,2,2,2,2, 2,2,2,2,2, 2,2,2,2,2, 2,2,0],
-              [0,2,0,0,0, 2,0,0,0,2, 0,0,0,2,0, 0,0,2,0,0, 0,2,0],
-              [0,2,0,0,0, 2,0,0,0,2, 0,0,0,2,0, 0,0,2,0,0, 0,2,0],
+              [0,2,4,4,4, 2,4,4,4,2, 4,4,4,2,4, 6,4,2,4,4, 4,2,0],
+              [0,2,4,4,4, 2,4,4,4,2, 4,4,4,2,4, 4,4,2,4,4, 4,2,0],
               [0,2,2,2,2, 2,2,2,2,2, 2,2,2,2,2, 2,2,2,2,2, 2,2,0],
-              [0,2,0,0,0, 2,0,0,0,2, 0,0,0,2,0, 0,0,2,0,0, 0,2,0],
-              [0,2,0,0,0, 2,0,0,0,2, 0,0,0,2,0, 0,0,2,0,0, 0,2,0],
+              [0,2,4,4,4, 2,4,4,4,2, 4,4,4,2,4, 4,4,2,4,4, 4,2,0],
+              [0,2,4,4,4, 2,4,4,4,2, 4,4,4,2,4, 4,4,2,4,4, 4,2,0],
               [0,2,2,2,2, 2,2,2,2,2, 2,2,2,2,2, 2,2,2,2,2, 2,2,0],
-              [0,2,0,0,0, 2,0,0,0,2, 0,0,0,2,0, 0,0,2,0,0, 0,2,0],
-              [0,2,0,0,0, 2,0,0,0,2, 0,0,0,2,0, 0,0,2,0,0, 0,2,0],
+              [0,2,4,4,4, 2,4,4,4,2, 4,4,4,2,4, 4,4,2,4,4, 4,2,0],
+              [0,2,4,4,4, 2,4,4,4,2, 4,4,4,2,4, 4,4,2,4,4, 4,2,0],
               [0,2,2,2,2, 2,2,2,2,2, 2,2,2,2,2, 2,2,2,2,2, 2,2,0],
-              [0,2,0,0,0, 2,0,0,0,2, 0,0,0,2,0, 0,0,2,0,0, 0,2,0],
-              [0,2,0,0,0, 2,0,0,0,2, 0,0,0,2,0, 0,0,2,0,0, 0,2,0],
+              [0,2,4,4,4, 2,4,4,4,2, 4,4,4,2,4, 4,4,2,4,4, 4,2,0],
+              [0,2,4,4,4, 2,4,4,4,2, 4,4,4,2,4, 4,4,2,4,4, 4,2,0],
               [0,2,2,2,2, 2,2,2,2,2, 2,2,2,2,2, 2,2,2,2,2, 2,2,0],
               [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0]
   ];
@@ -69,6 +42,7 @@ function resizeElements() {
 
 
 
+
   // Dibujar mapa
   function  mostrarMapa(){
     //Borro el contenido del div padre
@@ -77,12 +51,13 @@ function resizeElements() {
       mapa.forEach(function(element){
         for (var i = 0; i < element.length; i++) {
           var node = document.createElement("div");
-          if (element[i] == 0) {node.classList.add("pared");}
+          if (element[i] == 0 || element[i] == 4) {node.classList.add("pared");}
           else if(element[i] == 2){node.classList.add("camino");}
           else if(element[i] == 1){node.classList.add("personaje");}
           else if(element[i] == 3){node.classList.add("pisado");}
           else if(element[i] == 'S'){node.classList.add("score");}
           else if(element[i] == 'V'){node.classList.add("vidas");}
+          else if(element[i] == 6){node.classList.add("llave");}
           node.classList.add("casilla");
           mapa_div.appendChild(node);
         }
@@ -108,7 +83,7 @@ function resizeElements() {
     // Se modifica el array interno y el DOM
     function mover(dirreccion) {
       if(dirreccion=="arriba"){
-        if(mapa[personajeY-1][personajeX] != 0){
+        if(mapa[personajeY-1][personajeX] != 0 && mapa[personajeY-1][personajeX] != 4 && mapa[personajeY-1][personajeX] != 6){
           mapa[personajeY][personajeX]   = 3;
           mapa[personajeY-1][personajeX] = 1;
           arr_mapa[personajeY][personajeX].classList.remove('personaje');
@@ -119,7 +94,7 @@ function resizeElements() {
         }
       }
       else if(dirreccion=="abajo"){
-        if(mapa[personajeY+1][personajeX] != 0){
+        if(mapa[personajeY+1][personajeX] != 0 && mapa[personajeY+1][personajeX] != 4 && mapa[personajeY+1][personajeX] != 6){
           mapa[personajeY][personajeX]   = 3;
           mapa[personajeY+1][personajeX] = 1;
           arr_mapa[personajeY][personajeX].classList.remove('personaje');
@@ -130,7 +105,7 @@ function resizeElements() {
         }
       }
       else if(dirreccion=="der"){
-        if(mapa[personajeY][personajeX+1] != 0){
+        if(mapa[personajeY][personajeX+1] != 0 && mapa[personajeY][personajeX+1] != 4 && mapa[personajeY][personajeX+1] != 6){
           mapa[personajeY][personajeX]   = 3;
           mapa[personajeY][personajeX+1] = 1;
           arr_mapa[personajeY][personajeX].classList.remove('personaje');
@@ -141,7 +116,7 @@ function resizeElements() {
         }
       }
       else if(dirreccion=="izq"){
-        if(mapa[personajeY][personajeX-1] != 0){
+        if(mapa[personajeY][personajeX-1] != 0 && mapa[personajeY][personajeX-1] != 4 && mapa[personajeY][personajeX-1] != 6){
           mapa[personajeY][personajeX]   = 3;
           mapa[personajeY][personajeX-1] = 1;
           arr_mapa[personajeY][personajeX].classList.remove('personaje');
@@ -153,7 +128,36 @@ function resizeElements() {
       }
       resize();
       resizeElements();
+      checkCajas();
     }
+    function checkCajas() {
+      mirarAlrededor(2,3); // y,x de cada caja
+      mirarAlrededor(y,x);
+    }
+    function mirarAlrededor(y,x) {
+      if ( y+1 == 3
+        && y-2 == 3){
+
+      }
+    }
+    // function checkArround() {
+    //   //arriba
+    //   if (mapa[personajeY-1][personajeX] = 4) {
+    //     mirarBordes(params = {X:personajeY-1,Y:personajeX});
+    //   }
+    // }
+    // function mirarBordes(params) {
+    //   var x = params[0];
+    //   var y = params[1];
+    //   if (params[2]!= "undefined") {
+    //     var visitado = params[2];
+    //     console.log(params);
+    //   }
+    //   else {
+    //     mirarBordes({x:personajeY-1,y:personajeX-1,array:[x,y]});
+    //   }
+    // }
+
   // Inputs desde teclado
   document.onkeydown = function(e) {
     // s
@@ -173,7 +177,30 @@ function resizeElements() {
       mover("arriba");
     }
   }
+  // Set the size of elements to fit
+  function resize() {
+    // Map size
 
+    document.getElementById('mapa').style.gridTemplateRows = "repeat("+_FILAS+","+(window.innerHeight/_FILAS)+"px)";
+    document.getElementById('mapa').style.gridTemplateColumns = "repeat("+_COL+","+(window.innerWidth/_COL)+"px)";
+    // Character size
+    document.getElementsByClassName('personaje')[0].style.setProperty('--height-character',((window.innerHeight/_FILAS)*2.2+'px'));
+    // Character margin
+    document.getElementsByClassName('personaje')[0].style.setProperty('--margin-character',(-window.innerHeight/_FILAS+'px'+" 0px 0px 0px"));
+    // Llave size
+    // document.getElementsByClassName('llave')[0].style.setProperty('--margin-llave',("0px 0px 0px "+(-(window.innerWidth/_COL))+"px"));
+    document.getElementsByClassName('llave')[0].style.setProperty('--height-llave',(((window.innerHeight/_FILAS)*2)+'px'));
+  }
+  // Resize de las pisadas
+  function resizeElements() {
+    aux_array = document.getElementsByClassName('pisado');
+    for (var i = 0; i < aux_array.length; i++) {
+      // Pisada size
+      aux_array[i].style.setProperty('--height-pisado',((window.innerHeight/_FILAS)*2+'px'));
+      // Pisada margin
+      aux_array[i].style.setProperty('--margin-pisado',(-(window.innerHeight/_FILAS)+'px'+" 0px 0px 0px"));
+    }
+  }
 
 
 
